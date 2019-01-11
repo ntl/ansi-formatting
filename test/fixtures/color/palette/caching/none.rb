bd15d42d0c25bb9cca55e12c62056960f130efdd
Nathan Ladd <nathanladd+github@gmail.com>
2019-01-11 14:04:07 -0600

    Palette: Test Fixtures

---
+++ test/fixtures/color/palette/caching/none.rb
@@ -0,0 +1,35 @@
+module Fixtures
+  module Color
+    module Palette
+      class Caching
+        class None
+          include TestBench::Bootstrap::Fixture
+
+          attr_reader :palette
+          attr_reader :color_id
+
+          def initialize(palette, color_id)
+            @palette = palette
+            @color_id = color_id
+          end
+
+          def self.call(palette, color_id)
+            instance = new(palette, color_id)
+            instance.()
+          end
+
+          def call
+            palette.color_id?(color_id) or fail
+
+            color_1 = palette.get(color_id)
+            color_2 = palette.get(color_id)
+
+            test "Color retrievals are not cached" do
+              refute(color_1.equal?(color_2))
+            end
+          end
+        end
+      end
+    end
+  end
+end
