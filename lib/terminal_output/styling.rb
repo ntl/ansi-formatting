f76a920e6f5468b92b473aa6f773fd517dbdfdb0
Nathan Ladd <nathanladd+github@gmail.com>
2019-01-11 14:05:51 -0600

    Palette: Standard

--- lib/terminal_output/styling.rb
+++ lib/terminal_output/styling.rb
@@ -3,3 +3,6 @@ require 'stringio'
 require 'terminal_output/styling/sgr'
 
 require 'terminal_output/styling/color/mode/rgb'
+
+require 'terminal_output/styling/color/value'
+require 'terminal_output/styling/color/palette/standard'
--- lib/terminal_output/styling/controls.rb
+++ lib/terminal_output/styling/controls.rb
@@ -2,3 +2,6 @@ require 'terminal_output/styling/controls/sgr'
 
 require 'terminal_output/styling/controls/color/mode/rgb/gradient'
 require 'terminal_output/styling/controls/color/mode/rgb/scale'
+
+require 'terminal_output/styling/controls/color'
+require 'terminal_output/styling/controls/color/standard'
