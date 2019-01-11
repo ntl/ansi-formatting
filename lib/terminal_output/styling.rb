60060eb0561ffaaaa99434b5efb96ed613a6ad3a
Nathan Ladd <nathanladd+github@gmail.com>
2019-01-11 14:58:15 -0600

    Palette (8-bit): Standard

--- lib/terminal_output/styling.rb
+++ lib/terminal_output/styling.rb
@@ -4,6 +4,8 @@ require 'terminal_output/styling/sgr'
 
 require 'terminal_output/styling/color/mode/rgb'
 
-require 'terminal_output/styling/color/value'
+require 'terminal_output/styling/color/palette/value'
 require 'terminal_output/styling/color/palette/standard'
 require 'terminal_output/styling/color/palette/high_intensity'
+require 'terminal_output/styling/color/palette/eight_bit/value'
+require 'terminal_output/styling/color/palette/eight_bit/standard'
