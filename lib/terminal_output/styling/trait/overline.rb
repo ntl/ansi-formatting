930af5789a57a5e45e201979702ee9d58d11e6c7
Nathan Ladd <nathanladd+github@gmail.com>
2019-01-23 09:33:24 -0600

    Trait: Overline

---
+++ lib/terminal_output/styling/trait/overline.rb
@@ -0,0 +1,32 @@
+module TerminalOutput
+  module Styling
+    module Trait
+      module Overline
+        extend Trait
+
+        def self.variant?(variant)
+          codes.key?(variant)
+        end
+
+        def self.get(variant)
+          codes[variant]
+        end
+
+        def self.default_code
+          fetch(:on)
+        end
+
+        def self.reset_code
+          fetch(:off)
+        end
+
+        def self.codes
+          {
+            :on => SGR::Code.fetch(:overline),
+            :off => SGR::Code.fetch(:reset_overline)
+          }
+        end
+      end
+    end
+  end
+end
