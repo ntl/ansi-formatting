5bf592538e8961e2bf53518861403a050d47b8c7
Nathan Ladd <nathanladd+github@gmail.com>
2019-01-23 10:01:36 -0600

    Trait (Font): Intensity

---
+++ lib/terminal_output/styling/trait/font/intensity.rb
@@ -0,0 +1,31 @@
+module TerminalOutput
+  module Styling
+    module Trait
+      module Font
+        module Intensity
+          extend Trait
+
+          def self.variant?(variant)
+            codes.key?(variant)
+          end
+
+          def self.get(variant)
+            codes[variant]
+          end
+
+          def self.reset_code
+            fetch(:normal)
+          end
+
+          def self.codes
+            {
+              :bold => SGR::Code.fetch(:bold),
+              :faint => SGR::Code.fetch(:faint),
+              :normal => SGR::Code.fetch(:reset_intensity)
+            }
+          end
+        end
+      end
+    end
+  end
+end
