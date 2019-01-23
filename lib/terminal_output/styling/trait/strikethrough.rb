cabef95a485cfba159c6ddfb86d20d783f42bf8e
Nathan Ladd <nathanladd+github@gmail.com>
2019-01-23 09:23:04 -0600

    Trait: Strikethrough

---
+++ lib/terminal_output/styling/trait/strikethrough.rb
@@ -0,0 +1,28 @@
+module TerminalOutput
+  module Styling
+    module Trait
+      module Strikethrough
+        extend Trait
+
+        def self.variant?(variant)
+          codes.key?(variant)
+        end
+
+        def self.get(variant)
+          codes[variant]
+        end
+
+        def self.reset_code
+          fetch(:off)
+        end
+
+        def self.codes
+          {
+            :on => SGR::Code.fetch(:crossed_out),
+            :off => SGR::Code.fetch(:reset_crossed_out)
+          }
+        end
+      end
+    end
+  end
+end
