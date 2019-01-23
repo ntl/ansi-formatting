2ce2281ba725e5bb253a2753dffdc22a83fa2df6
Nathan Ladd <nathanladd+github@gmail.com>
2019-01-23 14:28:15 -0600

    Trait (Color): Reverse Video

---
+++ lib/terminal_output/styling/trait/color/reverse_video.rb
@@ -0,0 +1,30 @@
+module TerminalOutput
+  module Styling
+    module Trait
+      module Color
+        module ReverseVideo
+          extend Trait
+
+          def self.variant?(variant)
+            codes.key?(variant)
+          end
+
+          def self.get(variant)
+            codes[variant]
+          end
+
+          def self.reset_code
+            fetch(:off)
+          end
+
+          def self.codes
+            {
+              :on => SGR::Code.fetch(:negative_image),
+              :off => SGR::Code.fetch(:positive_image)
+            }
+          end
+        end
+      end
+    end
+  end
+end
