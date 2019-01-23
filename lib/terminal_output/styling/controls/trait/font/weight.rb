45d39d857236e1b5bc8d89e1ba29778c2a00ae5f
Nathan Ladd <nathanladd+github@gmail.com>
2019-01-23 10:01:36 -0600

    Trait (Font): Weight

---
+++ lib/terminal_output/styling/controls/trait/font/weight.rb
@@ -0,0 +1,41 @@
+module TerminalOutput
+  module Styling
+    module Controls
+      module Trait
+        module Font
+          module Weight
+            module Variant
+              def self.example
+                list[0]
+              end
+
+              def self.list
+                [
+                  :bold,
+                  :faint,
+                  :normal
+                ]
+              end
+            end
+
+            module Code
+              def self.example(variant=nil)
+                variant ||= Variant.example
+
+                map.fetch(variant)
+              end
+
+              def self.reset
+                Styling::SGR::Code.fetch(:reset_intensity)
+              end
+
+              def self.map
+                Styling::Trait::Font::Weight.codes
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
