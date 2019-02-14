e1ab77be21820105a987f2d27afa0f22c1e79958
Nathan Ladd <nathanladd+github@gmail.com>
2019-02-14 12:52:34 -0600

    Writer Device: Text

--- lib/terminal_output/styling/writer/device.rb
+++ lib/terminal_output/styling/writer/device.rb
@@ -12,6 +12,19 @@ module TerminalOutput
         end
         attr_writer :mode
 
+        def text(text)
+          bytes_written = 0
+
+          if mode == Mode.escape_sequence
+            bytes_written += io.write('m')
+
+            self.mode = Mode.text
+          end
+
+          bytes_written += io.write(text)
+          bytes_written
+        end
+
         module Mode
           def self.text
             :text
